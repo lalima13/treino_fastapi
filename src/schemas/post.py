67607d99adddf8330a685ab41fc6e611cb080e67
Lucas Lima <lucaslimatech98@gmail.com>
2026-06-10 20:57:52 -0300

    Atualizados a função datetime para AwareDatetime

--- src/schemas/post.py
+++ src/schemas/post.py
@@ -1,14 +1,13 @@
-from pydantic import BaseModel
-from datetime import datetime
+from pydantic import AwareDatetime, BaseModel
 
 class PostIn(BaseModel):
     title: str
     content: str
-    published_at: datetime | None = None
+    published_at: AwareDatetime | None = None
     published: bool= False
 
 class PostUpdateIn(BaseModel):
     title: str | None = None
     content: str | None = None
-    published_at: datetime | None = None
+    published_at: AwareDatetime | None = None
     published: bool | None = None    
--- src/views/post.py
+++ src/views/post.py
@@ -1,8 +1,7 @@
-from pydantic import BaseModel
-from datetime import datetime
+from pydantic import AwareDatetime, BaseModel
 
 class PostOut(BaseModel):
     id: int
     title: str
     content: str
-    published_at: datetime | None
+    published_at: AwareDatetime | None

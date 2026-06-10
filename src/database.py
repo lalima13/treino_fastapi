7e41c08040170d03b8f2f42d34eea781961ced4e
Lucas Lima <lucaslimatech98@gmail.com>
2026-06-10 19:52:53 -0300

    Atualizando as principais dependencias do projeto

--- src/database.py
+++ src/database.py
@@ -7,3 +7,8 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./blog.db")
 database = databases.Database(DATABASE_URL)
 metadata = sa.MetaData()
 engine = sa.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+if os.getenv("RENDER"):
+    engine = sa.create_engine(DATABASE_URL)
+else:
+    engine = sa.create_engine(DATABASE_URL, connect_arhs={"check_same_thread": False})

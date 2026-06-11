bdfebccad952ff78bec6bf7f9f7c5ccfb8306ecf
Lucas Lima <lucaslimatech98@gmail.com>
2026-06-10 21:47:57 -0300

    Adicionado o driver psycopg2 para o postgres do render

--- src/database.py
+++ src/database.py
@@ -6,9 +6,8 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./blog.db")
 
 database = databases.Database(DATABASE_URL)
 metadata = sa.MetaData()
-engine = sa.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 if os.getenv("RENDER"):
     engine = sa.create_engine(DATABASE_URL)
 else:
-    engine = sa.create_engine(DATABASE_URL, connect_arhs={"check_same_thread": False})
+    engine = sa.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
